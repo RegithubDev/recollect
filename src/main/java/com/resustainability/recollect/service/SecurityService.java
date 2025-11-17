0c9c1c6421ec2606981b5534d1788d9a8223ef2d
Kunal Vartak <regithub10@gmail.com>
2025-11-17 10:55:42 +0530

    refactor: remove db-call on user context retrieval due to "STATELESS"

--- src/main/java/com/resustainability/recollect/service/SecurityService.java
+++ src/main/java/com/resustainability/recollect/service/SecurityService.java
@@ -2,11 +2,7 @@ package com.resustainability.recollect.service;
 
 import com.resustainability.recollect.dto.response.IUserContext;
 import com.resustainability.recollect.entity.principal.UserPrincipal;
-import com.resustainability.recollect.exception.InvalidSessionException;
-import com.resustainability.recollect.repository.AdminUserRepository;
-import com.resustainability.recollect.repository.CustomerRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,23 +10,10 @@ import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @Service
 public class SecurityService {
-    private final CustomerRepository customerRepository;
-    private final AdminUserRepository adminUserRepository;
-
-    @Autowired
-    public SecurityService(
-            CustomerRepository customerRepository,
-            AdminUserRepository adminUserRepository
-    ) {
-        this.customerRepository = customerRepository;
-        this.adminUserRepository = adminUserRepository;
-    }
-
     /**
      * Retrieves the currently authenticated User entity.
      *
@@ -41,31 +24,6 @@ public class SecurityService {
      *         otherwise an empty {@link Optional}.
      */
     public Optional<IUserContext> getCurrentUser() {
-        return getCurrentContextUser()
-                .flatMap(contextUser -> {
-                    if (Boolean.TRUE.equals(contextUser.getIsAdmin())) {
-                        return adminUserRepository.loadUserById(contextUser.getId());
-                    }
-                    if (Boolean.TRUE.equals(contextUser.getIsCustomer())) {
-                        return customerRepository.loadUserById(contextUser.getId());
-                    }
-                    return Optional.empty();
-                })
-                .map(user -> {
-                    final LocalDateTime userTokenAt = user.getTokenAt();
-                    final LocalDateTime sessionTokenAt = getCurrentUserTokenAt()
-                            .orElseThrow(InvalidSessionException::new);
-                    if (null == userTokenAt || !userTokenAt
-                            .truncatedTo(ChronoUnit.SECONDS)
-                            .equals(sessionTokenAt.truncatedTo(ChronoUnit.SECONDS))
-                    ) {
-                        throw new InvalidSessionException();
-                    }
-                    return user;
-                });
-    }
-
-    public Optional<IUserContext> getCurrentContextUser() {
         return getCurrentUserPrincipal().map(UserPrincipal::getUser);
     }
 
