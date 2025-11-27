e12c5657c58229b705da27bb5b13da2501edaf81
Kunal Vartak <regithub10@gmail.com>
2025-11-27 17:28:01 +0530

    misc: use data.sql for DML, and schema.sql for DDL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+UPDATE backend_servicecategory SET url = '/recollect/v1/mobile/list-bio-waste-categories' WHERE id = 1;
+UPDATE backend_servicecategory SET url = '/recollect/v1/mobile/list-scrap-categories' WHERE id = 2;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -78,6 +78,3 @@ SET @sql := IF(
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
-
-UPDATE backend_servicecategory SET url = '/recollect/v1/mobile/list-bio-waste-categories' WHERE id = 1;
-UPDATE backend_servicecategory SET url = '/recollect/v1/mobile/list-scrap-categories' WHERE id = 2;
