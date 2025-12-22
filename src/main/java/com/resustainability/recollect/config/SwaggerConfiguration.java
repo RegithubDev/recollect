90513f5db3226a9cc84bd97d9753355187151cea
Kunal Vartak <regithub10@gmail.com>
2025-12-22 13:16:01 +0530

    Update SwaggerConfiguration.java

--- src/main/java/com/resustainability/recollect/config/SwaggerConfiguration.java
+++ src/main/java/com/resustainability/recollect/config/SwaggerConfiguration.java
@@ -5,15 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 
-import jakarta.servlet.http.HttpServletRequest;
-
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfiguration {
