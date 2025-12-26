23299ad77e3123c6f0e7f18a9f8e1ce780e9ebed
Kunal Vartak <regithub10@gmail.com>
2025-12-26 13:22:06 +0530

    misc: update

---
+++ src/main/java/com/resustainability/recollect/dto/payload/PayloadSelectedItem.java
@@ -0,0 +1,6 @@
+package com.resustainability.recollect.dto.payload;
+
+public record PayloadSelectedItem(
+        Long id,
+        Integer quantity
+) {}
--- src/main/java/com/resustainability/recollect/dto/response/TokenResponse.java
+++ src/main/java/com/resustainability/recollect/dto/response/TokenResponse.java
@@ -1,15 +1,12 @@
 package com.resustainability.recollect.dto.response;
 
 public record TokenResponse(
-     
-
 		boolean isActive,
         String token,
         String fullName,
         String email,
         String userType,
         long id,
-        String phone_number,
+        String phoneNumber,
         String platform
-        
 ) {}
